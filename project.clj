f0368ff32ed85a74701879ce804f511bc81d3093
geoffreymuchai <geoffreymuchai@gmail.com>
2015-04-23 13:02:35 +0300

    GM: Add missing deps and development profiles

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
 (defproject onaio/milia "0.1.0-SNAPSHOT"
   :description "The ona.io Clojure Web API"
   :dependencies [;; CORE MILIA REQUIREMENTS
+                 [cheshire "5.2.0"]
+                 [clj-http "1.0.1"]
+                 [environ "1.0.0"]
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2843"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [cheshire "5.2.0"]
-                 [clj-http "1.0.1"]
                  [sablono "0.3.1"]
                  [prismatic/dommy "0.1.2"]
                  [org.omcljs/om "0.8.8"]
@@ -18,32 +19,47 @@
                  [cljs-http "0.1.17"]
                  [com.cognitect/transit-cljs "0.8.188"]]
 
-  :plugins [[lein-cljsbuild "1.0.5"]
+  :plugins [[com.keminglabs/cljx "0.6.0" :exclusions [org.clojure/clojure]]
+            [lein-cljsbuild "1.0.5"]
             [lein-midje "3.1.3"]
-            [com.keminglabs/cljx "0.6.0" :exclusions [org.clojure/clojure]]]
+            [lein-environ "1.0.0"]]
+
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]
+                   :env {:debug-api? false
+                         :jetty-min-threads 10
+                         :jetty-max-threads 80}}
+             :uberjar {:env {:debug-api? false
+                             :jetty-min-threads 10
+                             :jetty-max-threads 80}}}
+
   :prep-tasks [["cljx" "once"] "javac" "compile"]
   :source-paths ["src/clj"
                  "src/cljs"
                  "target/generated/src/clj"
                  "target/generated/src/cljs"
                  "target/classes"]
-  :test-paths ["test/clj" "target/generated/test/clj"]
+  :test-paths ["tests/clj" "target/generated/tests/clj"]
 
-  :clean-targets ["out/ona" "out/milia" "out/milia.js"]
-
-  :cljx {:builds [{:source-paths ["src/cljx"]
-                   :output-path "target/generated/src/clj"
-                   :rules :clj}
-                  {:source-paths ["src/cljx"]
-                   :output-path "target/generated/src/cljs"
-                   :rules :cljs}]}
+  :clean-targets ["build/ona" "build/milia" "build/milia.js"]
   :cljsbuild {
     :builds [{:id "milia"
               :source-paths ["src/cljs"
                              "target/generated/src/cljs"]
               :compiler {
-                :output-to "out/milia.js"
-                :output-dir "out"
+                :output-to "build/milia.js"
+                :output-dir "build"
                 :optimizations :none
                 :cache-analysis true
-                :source-map true}}]})
+                :source-map true}}]}
+  :cljx {:builds [{:source-paths ["src/cljx"]
+                   :output-path "target/generated/src/clj"
+                   :rules :clj}
+                  {:source-paths ["src/cljx"]
+                   :output-path "target/generated/src/cljs"
+                   :rules :cljs}
+                  {:source-paths ["tests/cljx"]
+                   :output-path "target/generated/tests/clj"
+                   :rules :clj}
+                  {:source-paths ["tests/cljx"]
+                   :output-path "target/generated/tests/cljs"
+                   :rules :cljs}]})
