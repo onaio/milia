b15fae6a47f83780a0f231271d82f4b0974bb651
pld <peter@helioid.com>
2015-05-19 15:44:43 -0400

    PLD: remove snapshot

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
       [path]
       (str "resources/public/js/" path))
 
-(defproject onaio/milia "0.1.2-SNAPSHOT"
+(defproject onaio/milia "0.1.2"
   :description "The milia.io Clojure Web API"
   :dependencies [;; CORE MILIA REQUIREMENTS
                  [cheshire "5.2.0"]
