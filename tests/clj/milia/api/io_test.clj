440c3f7b43116802cc62aa8ec1ee50b0f5c665ce
pld <peter@helioid.com>
2015-06-30 18:04:18 -0700

    PLD: fix map var expecation

--- tests/clj/milia/api/io_test.clj
+++ tests/clj/milia/api/io_test.clj
@@ -23,9 +23,9 @@
   {:socket-timeout socket-timeout
    :conn-timeout connection-timeout
    :connection-manager connection-manager
-   :save-request? (env :debug-api?)
-   :debug (env :debug-api?)
-   :debug-body (env :debug-api?)})
+   :save-request? (env :debug-api)
+   :debug (env :debug-api)
+   :debug-body (env :debug-api)})
 
 (defn with-options [m] (merge options m))
 (def as-raw {:raw-response? true})
