9f4f4f763a2ac2fb6d1695fdd93316f73e126101
Okal Otieno <okal@justokal.com>
2015-09-09 17:37:55 +0300

    OO: Add tests

---
+++ tests/clj/milia/api/rest_services_test.clj
@@ -0,0 +1,37 @@
+(ns milia.api.rest-services-test
+  (:require [midje.sweet :refer :all]
+            [milia.api.rest-services :refer :all]
+            [milia.api.http :refer [parse-http]]
+            [milia.utils.remote :refer [make-url]]))
+
+(def xform-id 1)
+(def rest-service-id 1)
+(def base-url (make-url "restservices"))
+(def single-rest-service-url (make-url "restservices" rest-service-id))
+(def service-url "http://example.org/webhook")
+
+(fact "create calls parse-http with the correct parameters"
+  (create xform-id "generic_json" service-url) => :api-response
+  (provided
+   (parse-http :post
+               base-url
+               :http-options
+               {:form-params {:xform xform-id
+                              :name "generic_json"
+                              :service_url service-url}
+                :content-type :json}) => :api-response))
+
+(fact "delete calls parse-http with the correct parameters"
+  (delete rest-service-id) => :api-response
+  (provided
+   (parse-http :delete single-rest-service-url) => :api-response))
+
+(fact "get-all calls parse-http with the correct parameters"
+  (get-all) => :api-response
+  (provided
+   (parse-http :get base-url) => :api-response))
+
+(fact "get-by-id calls parse-http with the correct parameters"
+  (get-by-id rest-service-id) => :api-response
+  (provided
+   (parse-http :get single-rest-service-url) => :api-response))
