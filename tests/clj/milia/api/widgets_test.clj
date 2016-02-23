5976ed0929d601024502f46ec840dd614ad52ffb
Okal Otieno <okal@justokal.com>
2016-02-23 11:38:02 +0300

    OO: Fix test formatting

--- tests/clj/milia/api/widgets_test.clj
+++ tests/clj/milia/api/widgets_test.clj
@@ -16,20 +16,20 @@
       content-object-url "https://stage.ona.io/api/v1/forms/12345"]
 
   (fact "widgets/generate-content-object-url"
-    (let [{:keys [content_type content_id]} widget-definition]
-      (generate-content-object-url content_type content_id)
-      => content-object-url))
+        (let [{:keys [content_type content_id]} widget-definition]
+          (generate-content-object-url content_type content_id)
+          => content-object-url))
 
   (fact "widgets/create returns the API response"
-    (create widget-definition) => :some-widget
-    (provided
-     (parse-http :post
-                 widgets-url
-                 :http-options {:content-type :json
-                                :form-params (assoc widget-definition
-                                                    :content_object
-                                                    content-object-url)})
-     => :some-widget)))
+        (create widget-definition) => :some-widget
+        (provided
+         (parse-http :post
+                     widgets-url
+                     :http-options {:content-type :json
+                                    :form-params (assoc widget-definition
+                                                        :content_object
+                                                        content-object-url)})
+         => :some-widget)))
 
 (def dataview-id 1)
 (def dataview-filter-url (str widgets-url "?dataviewid=" dataview-id))
@@ -37,31 +37,31 @@
 (def xform-filter-url (str widgets-url "?xform=" xform-id))
 
 (facts "about widgets/list"
-  (fact "widgets/list returns the API response without filters"
-    (list) => :response
-    (provided
-     (parse-http :get widgets-url :http-options {:content-type :json})
-     => :response))
-  (fact "widgets/list returns the API response when filtered by dataview ID"
-    (list :dataview-id dataview-id) => :response
-    (provided
-     (parse-http :get dataview-filter-url :http-options {:content-type :json})
-     => :response))
-  (fact "widgets/list returns the API response when filtered by xform ID"
-    (list :xform-id xform-id) => :response
-    (provided
-     (parse-http :get xform-filter-url :http-options {:content-type :json})
-     => :response)))
+       (fact "widgets/list returns the API response without filters"
+             (list) => :response
+             (provided
+              (parse-http :get widgets-url :http-options {:content-type :json})
+              => :response))
+       (fact "widgets/list returns the API response when filtered by dataview ID"
+             (list :dataview-id dataview-id) => :response
+             (provided
+              (parse-http :get dataview-filter-url :http-options {:content-type :json})
+              => :response))
+       (fact "widgets/list returns the API response when filtered by xform ID"
+             (list :xform-id xform-id) => :response
+             (provided
+              (parse-http :get xform-filter-url :http-options {:content-type :json})
+              => :response)))
 
 (facts "about widgets/update"
-  (let [widget-id 1
-        single-widget-url (make-url "widgets" widget-id)
-        patch-map {:order 2
-                   :aggregation "mode"}]
-    (fact "widgets/update returns the API response"
-      (update widget-id patch-map) => :response
-      (provided
-       (parse-http :patch single-widget-url
-                   :http-options {:content-type :json
-                                  :form-params patch-map})
-       => :response))))
+       (let [widget-id 1
+             single-widget-url (make-url "widgets" widget-id)
+             patch-map {:order 2
+                        :aggregation "mode"}]
+         (fact "widgets/update returns the API response"
+               (update widget-id patch-map) => :response
+               (provided
+                (parse-http :patch single-widget-url
+                            :http-options {:content-type :json
+                                           :form-params patch-map})
+                => :response))))
