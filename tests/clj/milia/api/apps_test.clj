86ed068bdb834b864401c9b4d3f8ed3ffa2c74a5
Roy Rutto <royrutto@gmail.com>
2016-05-20 20:40:09 +0300

    RR: update test

--- tests/clj/milia/api/apps_test.clj
+++ tests/clj/milia/api/apps_test.clj
@@ -1,11 +1,17 @@
 (ns milia.api.apps-test
   (:require [midje.sweet :refer :all]
             [milia.api.apps :refer :all]
-            [milia.api.http :refer [parse-http]]
-            [milia.utils.remote :refer [make-rapidpro-url]]))
+            [milia.api.http :refer [parse-http]]))
+
+(def server "rapidpro-ona")
+(def endpoint "flows")
+(def api-key "a1b2c3")
 
 (fact "get-all calls parse-http with the correct parameters"
-      (get-rapidpro-flows "a1b2c3") => :api-response
+      (get-textit-data api-key endpoint server) => :api-response
       (provided
-       (make-rapidpro-url "flows") => :url
-       (parse-http :get :url :token "a1b2c3") => :api-response))
+       (make-textit-url server (str endpoint ".json")) => :url
+       (parse-http :get :url :http-options {:auth-token api-key}
+                   :as-map? true
+                   :raw-response? true
+                   :suppress-4xx-exceptions? true) => :api-response))
