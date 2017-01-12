a6923f0a2483334e72fa49339a413328cffbbbdc
pld <peetldee@gmail.com>
2017-01-12 18:45:20 -0500

    PLD: add warn on reflection

--- project.clj
+++ project.clj
@@ -69,4 +69,5 @@
                               ["phantomjs"
                                "phantom/unit-test.js"
                                "phantom/unit-test.html"
-                               "target/main-test.js"]}})
+                               "target/main-test.js"]}}
+  :global-vars {*warn-on-reflection* true})
