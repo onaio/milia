f47f8d27de9d643ba1726741c2de82d638338006
Mark Ekisa <mark.ekisa@gmail.com>
2019-12-03 16:58:00 +0300

    Fix formatting issue
    Signed-off-by: Mark Ekisa <mark.ekisa@gmail.com>

--- test/clj/milia/utils/images_test.clj
+++ test/clj/milia/utils/images_test.clj
@@ -3,15 +3,15 @@
             [milia.utils.images :refer :all]))
 
 (facts "about resize-image"
-        (fact "should return with image-url and edge dimension"
-                (resize-image "s3.aws.org" 80) =>
-                "https://images.ona.io/unsafe/80x80/smart/s3.aws.org")
+       (fact "should return with image-url and edge dimension"
+             (resize-image "s3.aws.org" 80) =>
+             "https://images.ona.io/unsafe/80x80/smart/s3.aws.org")
 
-        (fact "should return with image-url, width and height dimension"
-                (resize-image "s3.aws.org" 80 56)
-                => "https://images.ona.io/unsafe/80x56/smart/s3.aws.org")
+       (fact "should return with image-url, width and height dimension"
+             (resize-image "s3.aws.org" 80 56)
+             => "https://images.ona.io/unsafe/80x56/smart/s3.aws.org")
 
-        (fact "should return with image-url, width and height dimension as well
-                as optional image-server-url"
-                (resize-image "s3.aws.org" 80 56 "https://images.test.org")
-                => "https://images.test.org/unsafe/80x56/smart/s3.aws.org"))
+       (fact "should return with image-url, width and height dimension as well
+              as optional image-server-url"
+             (resize-image "s3.aws.org" 80 56 "https://images.test.org")
+             => "https://images.test.org/unsafe/80x56/smart/s3.aws.org"))
