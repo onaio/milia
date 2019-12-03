9281e48bf3bd6176d7573c364735e901bf9ef158
Mark Ekisa <mark.ekisa@gmail.com>
2019-12-03 16:35:16 +0300

    Add optional image-server-url var
    Signed-off-by: Mark Ekisa <mark.ekisa@gmail.com>

--- src/milia/api/images.clj
+++ src/milia/api/images.clj
@@ -7,11 +7,13 @@
 
 (defn upload
   "Process an avatar upload. Expects a binary avatar data object."
-  [file]
+  [file & [image-server-url]]
   (when (and file (:size file) (-> file :size zero? not))
     (let [{:keys [headers status]}
           (parse-http :post
-                      upload-url
+                      (or (and image-server-url
+                               (str image-server-url "/image"))
+                          upload-url)
                       :http-options
                       (merge
                        {:headers {"Slug" (:filename file)}}
@@ -20,4 +22,6 @@
                       :suppress-4xx-exceptions? true)]
       (when (= status 201)
         ;; A 201 HTTP status code indicates success
-        (str thumbor-server "/" (subs (headers "Location") 1))))))
+        (str (or image-server-url thumbor-server)
+             "/"
+             (subs (headers "Location") 1))))))
