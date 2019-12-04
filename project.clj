c1395069380842ce17a51d97c3b9077c485dde76
Mark Ekisa <mark.ekisa@gmail.com>
2019-12-04 15:49:16 +0300

    Remove the SNAPSHOT suffix
    Signed-off-by: Mark Ekisa <mark.ekisa@gmail.com>

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
    :milia-http-default-per-route "10"
    :milia-http-threads "20"})
 
-(defproject onaio/milia "0.3.53-SNAPSHOT"
+(defproject onaio/milia "0.3.53"
   :description "The ona.io Clojure Web API Client."
   :dependencies [;; CORE MILIA REQUIREMENTS
                  [cheshire "5.6.3"]
