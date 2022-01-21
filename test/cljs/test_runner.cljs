bcdb9681bd0b5a169d3dbd8d670647e8f5436e29
FrankApiyo <franklineapiyo@gmail.com>
2022-01-21 15:09:26 +0300

    Use lein-doo to run cljs tests instead of phantomjs
    Update dependancies

--- test/cljs/test_runner.cljs
+++ test/cljs/test_runner.cljs
@@ -1,6 +1,6 @@
 (ns test-runner
   (:require
-   [cljs.test :as test :refer-macros [run-tests] :refer [report]]
+   [doo.runner :refer-macros [doo-tests]]
    [milia.api.async-export-test]
    [milia.api.io-test]
    [milia.api.submissions-test]
@@ -8,19 +8,7 @@
 
 (enable-console-print!)
 
-(defmethod report [::test/default :summary] [m]
-  (println "\nRan" (:test m) "tests containing"
-           (+ (:pass m) (:fail m) (:error m)) "assertions.")
-  (println (:fail m) "failures," (:error m) "errors.")
-  (aset js/window "test-failures" (+ (:fail m) (:error m))))
-
-(defn runner []
-  (if (cljs.test/successful?
-       (run-tests
-        (test/empty-env ::test/default)
-        'milia.api.async-export-test
-        'milia.api.io-test
-        'milia.api.submissions-test
-        'milia.api.project-test))
-    0
-    1))
+(doo-tests 'milia.api.async-export-test
+           'milia.api.io-test
+           'milia.api.submissions-test
+           'milia.api.project-test)
