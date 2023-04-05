f49ee18b204f071a3780044650e56ec18cdee9cd
apiyo <franklineapiyo@gmail.com>
2023-04-05 16:11:31 +0300

    Update Cookies import in milia.api.io

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
    :milia-http-default-per-route "10"
    :milia-http-threads "20"})
 
-(defproject onaio/milia "0.9.1-rc"
+(defproject onaio/milia "0.9.2"
   :description "The ona.io Clojure Web API Client."
   :dependencies [;; CORE MILIA REQUIREMENTS
                  [cheshire "5.11.0"]
@@ -17,11 +17,11 @@
                  [onaio/chimera "0.1.2" :exclusions [log4j]]
                  [org.clojure/clojure "1.11.1"]
                  [org.clojure/tools.logging "1.2.4"]
-                 [com.fzakaria/slf4j-timbre "0.3.21"]
+                 [com.fzakaria/slf4j-timbre "0.4.0"]
                  ;;cljs
                  [cljs-hash "0.0.2"]
                  [org.clojure/clojurescript "1.11.60"]
-                 [org.clojure/core.async "1.5.648" :exclusions [org.clojure/tools.reader]]
+                 [org.clojure/core.async "1.6.673" :exclusions [org.clojure/tools.reader]]
                  [slingshot "0.12.2"]
                  ;; CLIENT REQUIREMENTS
                  [cljs-http "0.1.46" :exclusions [com.cognitect/transit-cljs]]]
@@ -42,7 +42,7 @@
   :eastwood {:exclude-linters [:constant-test :unused-locals :unused-fn-args :unused-private-vars]
              :add-linters [:unused-fn-args :unused-namespaces]
              :namespaces [:source-paths]}
-  :profiles {:dev {:dependencies [[midje "1.10.5" :exclusions [joda-time org.clojure/tools.namespace clj-time]]]
+  :profiles {:dev {:dependencies [[midje "1.10.9" :exclusions [joda-time org.clojure/tools.namespace clj-time]]]
                    :env ~project-env}
              :uberjar {:env ~project-env}}
   :test-paths ["test/clj" "target/generated/test/clj"]
